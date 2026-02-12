e4749be0872e457d54519044b84593f1385e76cb
Thomas Knutsen <knutsenthomas@gmail.com>
2026-02-12 23:53:16 +0100

    Fix: Revert video tool to standard config and aggressive spacing reset

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1261,8 +1261,8 @@ class AdminManager {
             } else console.warn('Editor.js: Delimiter tool missing');
 
             if (typeof Embed !== 'undefined') {
-                // alert('DEBUG SUCCESS: Embed-klassen er tilgjengelig!'); // Removing alert to reduce noise
-                toolsConfig.video = {
+                console.log('Editor.js: Embed tool loaded');
+                toolsConfig.embed = {
                     class: Embed,
                     inlineToolbar: true,
                     config: {
@@ -1273,15 +1273,12 @@ class AdminManager {
                         }
                     },
                     toolbox: {
-                        title: 'Video / YouTube',
-                        icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" fill="currentColor"/></svg>'
+                        title: 'YouTube / Video'
                     }
                 };
             } else {
-                console.error('Editor.js: Embed tool missing!');
+                console.error('Editor.js: Embed tool missing');
             }
-
-            console.log('Final toolsConfig:', toolsConfig); // Debug log
 
             const editor = new EditorJS({
                 holder: 'editorjs-container',
